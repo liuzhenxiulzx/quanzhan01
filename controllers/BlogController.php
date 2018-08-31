21166a0eae6f75694c5092ea3bf221f4168fa0ec
unknown <lzx@lzx.com>
2018-08-31 15:35:01 +0800

    页面显示

---
+++ controllers/BlogController.php
@@ -0,0 +1,137 @@
+<?php
+namespace controllers;
+
+use PDO;
+
+class BlogController
+{
+    // 日志列表
+    public function index()
+    {
+        // 取日志的数据
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=blog','root','');
+        $pdo->exec('SET NAMES UTF8');
+
+        // 设置的 $where
+        $where = 1;
+
+        // 放预处理对应的值
+        $value = [];
+        
+        // 如果有keword 并值不为空时
+        if(isset($_GET['keyword']) && $_GET['keyword'])
+        {
+            $where .= " AND (title LIKE ? OR content LIKE ?)";
+            $value[] = '%'.$_GET['keyword'].'%';
+            $value[] = '%'.$_GET['keyword'].'%';
+        }
+
+        if(isset($_GET['start_date']) && $_GET['start_date'])
+        {
+            $where .= " AND created_at >= ?";
+            $value[] = $_GET['start_date'];
+        }
+
+        if(isset($_GET['end_date']) && $_GET['end_date'])
+        {
+            $where .= " AND created_at <= ?";
+            $value[] = $_GET['end_date'];
+        }
+
+        if(isset($_GET['is_show']) && ($_GET['is_show']==1 || $_GET['is_show']==='0'))
+        {
+            $where .= " AND is_show = ?";
+            $value[] = $_GET['is_show'];
+        }
+
+
+        /***************** 排序 ********************/
+        // 默认排序
+        $odby = 'created_at';
+        $odway = 'desc';
+
+        if(isset($_GET['odby']) && $_GET['odby'] == 'display')
+        {
+            $odby = 'display';
+        }
+
+        if(isset($_GET['odway']) && $_GET['odway'] == 'asc')
+        {
+            $odway = 'asc';
+        }
+
+        /****************** 翻页 ****************/
+        $perpage = 15; // 每页15
+        // 接收当前页码（大于等于1的整数）， max：最参数中大的值
+        $page = isset($_GET['page']) ? max(1,(int)$_GET['page']) : 1;
+        // 计算开始的下标
+        // 页码  下标
+        // 1 --> 0
+        // 2 --> 15
+        // 3 --> 30
+        // 4 --> 45
+        $offset = ($page-1)*$perpage;
+
+        // 制作按钮
+        // 取出总的记录数
+        $stmt = $pdo->prepare("SELECT COUNT(*) FROM blog WHERE $where");
+        $stmt->execute($value);
+        $count = $stmt->fetch( PDO::FETCH_COLUMN );
+        // 计算总的页数（ceil：向上取整（天花板）， floor：向下取整（地板））
+        $pageCount = ceil( $count / $perpage );
+
+        $btns = '';
+        for($i=1; $i<=$pageCount; $i++)
+        {
+            // 先获取之前的参数
+            $params = getUrlParams(['page']);
+
+            $class = $page==$i ? 'active' : '';
+            $btns .= "<a class='$class' href='?{$params}page=$i'> $i </a>";
+            
+        }
+
+        /*************** 执行 sqL */
+        // 预处理 SQL
+        $stmt = $pdo->prepare("SELECT * FROM blog WHERE $where ORDER BY $odby $odway LIMIT $offset,$perpage");
+        // 执行 SQL
+        $stmt->execute($value);
+
+        // 取数据
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // print_r($data);
+        // 加载视图
+        view('blogs.index', [
+            'data' => $data,
+            'btns' => $btns,
+        ]);
+    }
+
+    public function content_to_html()
+    {
+        // 取日志的数据
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=blog','root','');
+        $pdo->exec('SET NAMES UTF8');
+
+        $stmt = $pdo->query('SELECT * FROM blog');
+        $blogs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // 开启缓冲区
+        ob_start();
+
+        // 生成静态页
+        foreach($blogs as $v)
+        {
+            // 加载视图
+            view('blogs.content', [
+                'blog' => $v,
+            ]);
+            // 取出缓冲区的内容
+            $str = ob_get_contents();
+            // 生成静态页
+            file_put_contents(ROOT.'public/contents/'.$v['id'].'.html', $str);
+            // 清空缓冲区
+            ob_clean();
+        }
+    }
+}
